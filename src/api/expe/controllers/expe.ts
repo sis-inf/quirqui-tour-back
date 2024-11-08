f23a0cdb6be9387266ae4443fe8414c5aa6fcf9e
MrRobot <juaniquinapokar@gmail.com>
2024-11-08 17:48:09 -0400

    prueba2

---
+++ src/api/expe/controllers/expe.ts
@@ -0,0 +1,7 @@
+/**
+ * expe controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::expe.expe');
---
+++ src/api/expe/routes/expe.ts
@@ -0,0 +1,7 @@
+/**
+ * expe router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::expe.expe');
